160745586c07d37e5eabfa4dc14ab9cf61a33fbd
E2eDigitech <it@e2edigitech.com>
2025-09-23 14:34:27 +0530

    change

--- seeders/20250813120559-demo-user.js
+++ seeders/20250813120559-demo-user.js
@@ -16,6 +16,6 @@ module.exports = {
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('users', { email: 'admin@example.com' }, {});
+    await queryInterface.bulkDelete('users', { email: 'dmin@visibeen.com' }, {});
   }
 };
